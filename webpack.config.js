1f3b623619cd499ba9894dcb337fc1fc38923c3f
Craig Guest <craigguest@outlook.com>
2020-03-11 00:20:33 +0000

    Added configuration for minimising javascript, this resolves #12.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-const path = require('path');
+const path = require('path'),
+      TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    devtool: false,
+    devtool: 'source-map',
     entry: {
         "background": './src/background.js',
         "options": './src/options.js',
@@ -9,5 +10,18 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'extension/scripts')
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true,
+                terserOptions: {
+                    compress: {
+                        drop_console: true
+                    }
+                }
+            })
+        ]
     }
 };
